2d11c0cbdc2c642bce9ef4538d235f08c9069d79
EnergoStalin <ens.stalin@gmail.com>
2026-02-27 20:12:30 +0300

    chore(db/model): add CASCADE & unique on user.id foreign_key

--- services/api/src/api/db/models/RefreshToken.py
+++ services/api/src/api/db/models/RefreshToken.py
@@ -6,9 +6,16 @@ from sqlmodel import Field, SQLModel
 
 
 class RefreshToken(SQLModel, table=True):
-    user_id: UUID = Field(foreign_key="user.id", nullable=False)
+    user_id: UUID = Field(
+        foreign_key="user.id",
+        nullable=False,
+        ondelete="CASCADE",
+        unique=True,
+        index=True,
+    )
     id: int | None = Field(default=None, primary_key=True, nullable=False)
     token: str = Field(max_length=1024, nullable=False)
     expire_at: datetime = Field(
-        nullable=False, sa_type=DateTime(timezone=True)  # pyright: ignore[reportArgumentType]
+        nullable=False,
+        sa_type=DateTime(timezone=True),  # pyright: ignore[reportArgumentType]
     )
