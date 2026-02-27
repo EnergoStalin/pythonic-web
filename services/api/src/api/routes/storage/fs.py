af5929595ff49e7f862e5c7330243d0e76d181bc
EnergoStalin <ens.stalin@gmail.com>
2026-02-27 18:43:14 +0300

    chore(api/storage): add pagination

--- services/api/src/api/routes/storage/fs.py
+++ services/api/src/api/routes/storage/fs.py
@@ -1,11 +1,10 @@
+import aiofiles
 from api.routes.storage.config import SPOOL_PATH
 from fastapi import UploadFile
 
-import aiofiles
-
 
 async def save_file_chunked(
-    uf: UploadFile, chunk_size: int = 1024 * 1024  # 1MB chunks
+    uf: UploadFile, chunk_size: int = 1024 * 1024
 ):
     if not uf.filename:
         raise ValueError("Filename is None")
@@ -13,12 +12,10 @@ async def save_file_chunked(
     fp = SPOOL_PATH.joinpath(uf.filename)
 
     try:
-        # Using aiofiles for async file writing
         async with aiofiles.open(fp, "wb") as f:
             while chunk := await uf.read(chunk_size):
                 _ = await f.write(chunk)
     except Exception as e:
-        # Clean up if something goes wrong
         if fp.exists():
             fp.unlink()
         raise e
--- services/api/src/api/routes/storage/storage.py
+++ services/api/src/api/routes/storage/storage.py
@@ -1,21 +1,26 @@
 import asyncio
 import mimetypes
-from os import listdir
+from http import HTTPStatus
+from itertools import islice
+from os import scandir
 from pathlib import Path as FSPath
 from typing import Annotated
 from urllib.parse import quote, urljoin
 
 from api.config import BASE_URL
 from api.models.FileInfo import FileInfo
+from api.models.StorageConfig import StorageConfig
 from api.routes.storage.config import SPOOL_PATH
 from api.routes.storage.fs import save_file_chunked
 from fastapi import UploadFile
-from fastapi.params import File, Path
+from fastapi.params import File, Path, Query
 from fastapi.requests import Request
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, Response
 from fastapi.routing import APIRouter
 
 router = APIRouter()
+
+ACCEPTED_EXTENSIONS = [".png", ".mp4"]
 
 
 def guess_mime(fn: str):
@@ -23,30 +28,49 @@ def guess_mime(fn: str):
     return tp or "application/octet-stream"
 
 
-@router.get("")
-async def list_files(request: Request):
+@router.get("/files")
+async def list_files(
+    request: Request,
+    page: Annotated[int, Query()] = 1,
+    limit: Annotated[int, Query()] = 20,
+):
     prefix = FSPath(request.url.path)
-    print(BASE_URL, prefix)
+
+    start = (page - 1) * limit
+    end = start + limit
+    slice = islice(filter(lambda fp: fp.is_file(), scandir(SPOOL_PATH)), start, end)
 
     return [
         FileInfo(
-            name=fn,
-            mime=guess_mime(fn),
-            url=urljoin(BASE_URL, prefix.joinpath(quote(fn)).as_posix()),
+            name=d.name,
+            mime=guess_mime(d.name),
+            url=urljoin(BASE_URL, prefix.joinpath(quote(d.name)).as_posix()),
         )
-        for fn in listdir(SPOOL_PATH)
+        for d in slice
     ]
 
 
-@router.get("/{name}")
+@router.get("/files/{name}")
 async def get_file(name: Annotated[str, Path()]):
     path = SPOOL_PATH.joinpath(name)
     return FileResponse(path, media_type=guess_mime(path.as_posix()))
 
 
 # Было бы время написал бы свою реализацию но осталось около 10 часов а я уже почти никакой хоть и встал в 7 утра так что так
-@router.post("")
+@router.post("/files")
 async def upload_files(
     files: Annotated[list[UploadFile], File(description="Multiple files")],
 ):
+    for uf in filter(lambda uf: uf.filename, files):
+        if (
+            suffix := FSPath(uf.filename).suffix  # pyright: ignore[reportArgumentType]
+        ) not in ACCEPTED_EXTENSIONS:
+            return Response(f"suffix {suffix} not allowed", HTTPStatus.FORBIDDEN)
+
     _ = await asyncio.gather(*[save_file_chunked(uf) for uf in files])
+    return ""
+
+
+@router.get("/config")
+async def config():
+    return StorageConfig(accept=ACCEPTED_EXTENSIONS)
